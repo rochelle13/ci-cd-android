92080566d631b178fb92fd7ded021ba9ef10520e
unknown <rochellemoodley13@gmail.com>
2023-10-25 20:27:29 +0000

    first commit

--- app/src/main/java/dev/idee/cicdandroid/MainActivityViewModel.kt
+++ app/src/main/java/dev/idee/cicdandroid/MainActivityViewModel.kt
@@ -1,7 +1,6 @@
 package dev.idee.cicdandroid
 
 import android.util.Log
-import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import retrofit2.Call
